e7f509113cdf42b803adcffd452922d2eef40bb4
mjgomezv <lsx088@rescomp2.hpc.in.bmrc.ox.ac.uk>
2020-07-16 11:41:58 +0100

    check updates

--- R/calculate_qc_metrics.R
+++ R/calculate_qc_metrics.R
@@ -1,5 +1,5 @@
 # Libraries ------
-stopifnot(require(optparse), 
+stopifnot(require(optparse),
           require(futile.logger),
           require(dplyr),
           require(DropletUtils),
@@ -22,15 +22,15 @@ option_list <- list(
   make_option(c("--sample"), default=NULL,
               help="sample or channel id"),
   make_option(c("--barcodes_to_label_as_True"), default=NULL,
-              help="A header-less, two-column, gziped tsv.gz file with barcodes to label. 
+              help="A header-less, two-column, gziped tsv.gz file with barcodes to label.
               The name of the file will be used to set the column name in the output dataframe ('.tsv' will be removed from the final name).
               First column: barcode name in the format UMI-1, e.g AAACCTGAGAGGTACC-1.
               Second column: sample or channel id name.
               If a barcode is included in the input file, it will be labeled as 'True', otherwise as 'False'"),
   make_option(c("--genesets_file"), default=NULL,
-              help="Two-column tsv file with genesets to evaluate, one geneset per row. 
-              First column: name of geneset; 
-              Second column: name of the file containing the geneset. 
+              help="Two-column tsv file with genesets to evaluate, one geneset per row.
+              First column: name of geneset;
+              Second column: name of the file containing the geneset.
               The file containing the geneset must be a header-less, one-column tsv file with gene names (one per row)."),
   make_option(c("--numcores"), default=2,
               help="Number of cores used to run scater's perCellQCMetrics function"),
@@ -75,11 +75,11 @@ ngenes <- colSums(counts(s) > 0)
 # Total UMI counts
 total_UMI <- colSums(counts(s))
 
-# Create dataframe 
-cell_qc <- tibble(barcode = colData(s)$Barcode, 
+# Create dataframe
+cell_qc <- tibble(barcode = colData(s)$Barcode,
                   sample=opt$sample,
-                  ngenes = ngenes, 
-                  total_UMI= total_UMI) 
+                  ngenes = ngenes,
+                  total_UMI= total_UMI)
 
 
 # Compute percentage of genesets ------
@@ -92,51 +92,78 @@ genesets <- list()
 
 # Mitochondrial genes
 genesets$pct_mitochondrial <- grep("^MT-", rowData(s)$Symbol, ignore.case=TRUE)
-flog.info("Including %s mitochondrial genes for pct_mitochondrial", 
+flog.info("Including %s mitochondrial genes for pct_mitochondrial",
           length(genesets$pct_mitochondrial))
 
 # Ribosomal protein genes
 genesets$pct_ribosomal <- grep("^RPS|^RPL", rowData(s)$Symbol, ignore.case=TRUE)
-flog.info("Including %s ribosomal protein genes for pct_ribosomal", 
+flog.info("Including %s ribosomal protein genes for pct_ribosomal",
           length(genesets$pct_ribosomal))
 
 # Immunoglobin genes
-genesets$pct_immunoglobin <- grep("^IGH|^IGLC|^IGLJ|^IGLL|^IGLV|^IGK", 
+genesets$pct_immunoglobin <- grep("^IGH|^IGLC|^IGLJ|^IGLL|^IGLV|^IGK",
                                   rowData(s)$Symbol, ignore.case=TRUE)
-flog.info("Including %s immunoglobin genes for pct_immunoglobin", 
+flog.info("Including %s immunoglobin genes for pct_immunoglobin",
           length(genesets$pct_immunoglobin))
 
 # Hemoglobin genes
 genesets$pct_hemoglobin <- grep("^Hbb|^Hba|^Hbq", rowData(s)$Symbol, ignore.case=TRUE)
-flog.info("Including %s hemoglobin genes for pct_hemoglobin", 
+flog.info("Including %s hemoglobin genes for pct_hemoglobin",
           length(genesets$pct_hemoglobin))
+
+# Add genesets from cellqc package
+gs <- c("geneset.neutrophil", "geneset.platelet", "geneset.endothelial", "geneset.apoptotic")
+gs.names <- c("pct_neutrophil", "pct_platelet", "pct_endothelial", "pct_apoptotic")
+for (g in 1:length(gs)){
+  # Load geneset
+  geneset_name <- gs.names[g]
+  data(list=gs[g])
+  geneset <- get(gs[g])
+
+  flog.info("Geneset %s contains %s genes",
+            geneset_name, format(length(geneset), big.mark=","))
+
+  # Add to 'genesets' list
+  genesets[[geneset_name]] <- which(rowData(s)$Symbol %in% geneset)
+  flog.info("Including %s genes for %s",
+            format(length(genesets[[geneset_name]]), big.mark=","), geneset_name)
+
+  # Report genes not found
+  if ( sum(!geneset %in% rowData(s)$Symbol) >= 1) {
+    not_found <- geneset[-which(geneset %in% rowData(s)$Symbol)]
+    not_found <- paste0(not_found, collapse = ", ")
+    flog.info("Genes not found in count matrix: %s",
+              not_found)
+  }
+}
+
 
 # Add any other genesets provided
 if (!is.null(opt$genesets_file)){
-  
+
   flog.info("Reading genesets file %s", opt$genesets_file)
   provided_genesets <- read.csv(opt$genesets_file, sep="\t", header=FALSE)
-  
+
   for (i in 1:nrow(provided_genesets)) {
-    
+
     geneset_name <- provided_genesets[i,1]
-    
+
     # Read geneset file
     flog.info("Reading %s geneset file", geneset_name)
     geneset <- read.csv(provided_genesets[i,2], sep="\t", header=FALSE)[,1]
-    flog.info("Geneset %s contains %s genes", 
+    flog.info("Geneset %s contains %s genes",
               geneset_name, format(length(geneset), big.mark=","))
-    
+
     # Add to 'genesets' list
     genesets[[geneset_name]] <- which(rowData(s)$Symbol %in% geneset)
-    flog.info("Including %s genes for %s", 
+    flog.info("Including %s genes for %s",
               format(length(genesets[[geneset_name]]), big.mark=","), geneset_name)
-    
+
     # Report genes not found
     if ( sum(!geneset %in% rowData(s)$Symbol) >= 1) {
       not_found <- geneset[-which(geneset %in% rowData(s)$Symbol)]
       not_found <- paste0(not_found, collapse = ", ")
-      flog.info("Genes not found in count matrix: %s", 
+      flog.info("Genes not found in count matrix: %s",
                 not_found)
     }
   }
@@ -162,7 +189,7 @@ cell_qc <- left_join(cell_qc, pct_genesets, by = "barcode")
 
 # create matrix with gene symbol as rownames
 m <- counts(s)
-rnames <- left_join(data.frame(ID=rownames(m)), 
+rnames <- left_join(data.frame(ID=rownames(m)),
                     as.data.frame(rowData(s)),
                     by="ID")
 if (length(rownames(m)) != nrow(rnames)) {
@@ -182,7 +209,7 @@ cell_qc <- left_join(cell_qc, mitoribo_ratio, by = "barcode")
 # Label barcodes ------
 #######################
 if (!is.null(opt$barcodes_to_label_as_True)){
-  bc <- read.csv(gzfile(opt$barcodes_to_label_as_True), sep="\t", header=FALSE) 
+  bc <- read.csv(gzfile(opt$barcodes_to_label_as_True), sep="\t", header=FALSE)
   colnames(bc) <- c("barcode", "sample")
   # Filter barcodes from current sample
   bc <- bc[bc$sample %in% opt$sample,]
