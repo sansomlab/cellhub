fa3387bc02cc2b7de113acf06f22077083acfa6a
Steve Sansom <stephen.sansom@kennedy.ox.ac.uk>
2022-06-25 13:24:19 +0100

    Add mempercore support

--- cellhub/pipeline_cellranger_multi.py
+++ cellhub/pipeline_cellranger_multi.py
@@ -312,14 +312,22 @@ def cellrangerMulti(infile, outfile):
 
     log_file = id_tag + ".log"
 
+    mempercore = PARAMS["cellranger_mempercore"]
+
+    if mempercore:
+        mempercore_stat="--mempercore " + str(mempercore)
+    else:
+        mempercore_stat = ""
+
     # this statement is to run in slurm mode
     statement = '''cd cellranger.multi.dir;
                     cellranger multi
-	    	    --id %(id_tag)s
+	    	        --id %(id_tag)s
                     --csv=%(config_path)s
                     --jobmode=%(cellranger_job_template)s
                     --maxjobs=%(max_jobs)s
-		    --nopreflight
+		            --nopreflight
+                    %(mempercore_stat)s
                     &> %(log_file)s
                  '''
 
